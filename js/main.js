ea0fd91e709c4326f5a0062c659f4e5adcb8c805
okapeeokapee <okapee420@gmail.com>
2020-09-06 03:28:36 +0900

    ahh,, JS does not respond

--- js/main.js
+++ js/main.js
@@ -1,26 +1,14 @@
 'use strict';
 
 {
-  const open= document.getElementById('open');
-  const close= document.getElementById('close');
-  const modal= document.getElementById('modal');
-  const mask= document.getElementById('mask');
-  
-  open.addEventListener('click',()=>{
-    modal.classList.remove('hidden');
-    mask.classList.remove('hidden');
-  });
-
-  close.addEventListener('click',()=>{
-    modal.classList.add('hidden');
-    mask.classList.add('hidden');
-  });
-
-  mask.addEventListener('click',()=>{
-    // modal.classList.add('hidden');
-    // mask.classList.add('hidden');
-    close.click();
-  });
+  function changeDisplay(){
+    var str = document.getElementById("target");
+    if(str.style.display == "none"){
+      str.style.display = "block";
+    }else{
+      str.style.display = "none";
+    }
+  }
 }
 
 

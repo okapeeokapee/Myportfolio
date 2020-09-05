ec4e96323f6d6fa7cbfcc0ab3d81ebc4c34bf00d
okapeeokapee <okapee420@gmail.com>
2020-09-06 03:58:52 +0900

    basic completed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 'use strict';
 
 {
-  function changeDisplay(){
-    var str = document.getElementById("target");
-    if(str.style.display == "none"){
-      str.style.display = "block";
-    }else{
-      str.style.display = "none";
-    }
-  }
+
 }
 
 

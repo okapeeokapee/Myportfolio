6808017696af31b9b0379b3d5afc474aa3fca971
okapeeokapee <okapee420@gmail.com>
2020-11-05 22:39:54 +0900

    kesu.toggle add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 'use strict';{
-  
+  const okane =  document.getElementById('okane');
+  const okane1 = document.getElementById('okane1');
+
+  okane.addEventListener('click', () =>{
+    okane1.classList.toggle('kesu');
+  });
+
 }
 
 

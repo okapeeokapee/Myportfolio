5253c5332a5f30b728aab7873d3a86a84ed2eee4
okapeeokapee <okapee420@gmail.com>
2020-11-06 00:20:05 +0900

    JS add each books

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 'use strict';{
-  const okane =  document.getElementById('okane');
-  const okane1 = document.getElementById('okane1');
-
-  okane.addEventListener('click', () =>{
+  document.getElementById('ichi').addEventListener('click', () =>{
+    ichi1.classList.toggle('kesu');
+  });
+  document.getElementById('okane').addEventListener('click', () =>{
     okane1.classList.toggle('kesu');
+  });
+  document.getElementById('zibu').addEventListener('click', () =>{
+    zibu1.classList.toggle('kesu');
+  });
+  document.getElementById('tado').addEventListener('click', () =>{
+    tado1.classList.toggle('kesu');
+  });
+  document.getElementById('dare').addEventListener('click', () =>{
+    dare1.classList.toggle('kesu');
+  });
+  document.getElementById('naze').addEventListener('click', () =>{
+    naze1.classList.toggle('kesu');
+  });
+  document.getElementById('niho').addEventListener('click', () =>{
+    niho1.classList.toggle('kesu');
+  });
+  document.getElementById('hattari').addEventListener('click', () =>{
+    hattari1.classList.toggle('kesu');
+  });
+  document.getElementById('memo').addEventListener('click', () =>{
+    memo1.classList.toggle('kesu');
+  });
+  document.getElementById('mochi').addEventListener('click', () =>{
+    mochi1.classList.toggle('kesu');
   });
 
 }
